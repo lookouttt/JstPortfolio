a0c1d4a0750b9c36a8dd3682d51c7974d45376bc
lookouttt <lookoutt@hotmail.com>
2023-04-13 17:22:40 -0700

    Initial commit of parcel-based React portfolio project with Tailwind CSS.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// index.js
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<h1 class="text-3xl font-bold underline">Hello world!</h1>, document.getElementById("root"))
