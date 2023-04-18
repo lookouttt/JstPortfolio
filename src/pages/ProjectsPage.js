e6a10ccf65d7df9ebf44a285a9ec758b3eac40c3
lookouttt <lookoutt@hotmail.com>
2023-04-18 15:25:47 -0700

    Project page changes

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -1,13 +1,13 @@
-import jst_pic from '../app/assets/img/jst_logo.png';
+import jst_pic from '../app/assets/img/jst_round.png';
 import hist_pic from '../app/assets/img/music_historeum.jpg';
 import dbd_pic from '../app/assets/img/DBD.jpg';
 
 const ProjectsPage = () => {
     return (
-        <div>
-            <h1 className="mx-5 py-5 px-2 flex-initial w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">My Projects</h1>
+        <div className="bg-gradient-to-b from-stone-900 from-10% via-stone-400 via-40% to-stone-200 to-90%">
+            <h1 className="mx-5 py-5 px-2 flex-initial w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-slate-50">My Projects</h1>
             <div className="container mx-auto grid grid-cols-3">
-                <div className="container mx-auto overflow-hidden my-10">
+                <div className="container mx-auto overflow-hidden my-10 p-3">
                     <div className="max-w-sm rounded overflow-hidden shadow-lg">
                         <img className="w-full" src={jst_pic} alt="Sunset in the mountains"/>
                         <div className="px-6 py-4">
@@ -18,7 +18,7 @@ const ProjectsPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="container mx-auto overflow-hidden my-10">
+                <div className="container mx-auto overflow-hidden my-10 p-3">
                     <div className="max-w-sm rounded overflow-hidden shadow-lg">
                         <img className="w-full" src={hist_pic} alt="Sunset in the mountains"/>
                         <div className="px-6 py-4">
@@ -29,7 +29,7 @@ const ProjectsPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="container mx-auto overflow-hidden my-10">
+                <div className="container mx-auto overflow-hidden my-10 p-3">
                     <div className="max-w-sm rounded overflow-hidden shadow-lg">
                         <img className="w-full" src={dbd_pic} alt="Sunset in the mountains"/>
                         <div className="px-6 py-4">
