929857856183560b6f0718d37836f86a7136d302
lookouttt <lookoutt@hotmail.com>
2023-04-17 17:22:46 -0700

    Improved styling of About and Landing pages. Updated images.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,17 +3,17 @@ import fej_pic from '../app/assets/img/profile_pic.jpg';
 const AboutPage = () => {
     return (
         <div id="about_me">
-            <div className="h-24 bg-gradient-to-b from-stone-400 via-stone-600 to-stone-700"></div>
+            {/* <div className="h-32 bg-gradient-to-b from-stone-300  to-stone-700"></div> */}
             <div className="w-[100vw] h-[40vh] sm:h-[50vh] md:h-[70vh] lg:h-[80vh] xl:h-[90vh] 2xl:h-[100vh]
-                            bg-no-repeat bg-cover bg-gradient-to-b from-stone-700 via-stone-800 to-stone-900">
+                            bg-no-repeat bg-cover bg-gradient-to-b from-stone-600 via-stone-700 to-stone-800">
                             
                 <div className="container m-auto grid grid-cols-2">
+                    <div className="container mx-auto max-w-sm overflow-hidden my-10 sm:my-20">
+                        <img className="rounded-full h-96" src={fej_pic} />
+                    </div>
                     <div className="container mx-auto text-gray-400 font-roboto_slab my-10 sm:my-20">
                         <h1 className="py-5 px-2 flex-initial w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">About Me.</h1>
                         <h1 className="py-5 px-2 flex-initial w-100 text-lg sm:text-xl md:text-2xl">I am a former software engineer from the material handling industry now working within the web and app development space. Working in the material handling industry, I dealt with many complex situations in order to move boxes from point A to point B. My 20 years experience in that arena helped me to hone my ability to work through any challenges necessary to meet client expectations. Reach out and let me take that experience and put it to work for you.</h1>
-                    </div>
-                    <div class="max-w-sm rounded-lg overflow-hidden shadow-lg my-10 sm:my-20">
-                        <img class="w-full" src={fej_pic} />
                     </div>
                 </div>
             </div>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,15 +1,23 @@
-import bg_landing from '../app/assets/img/lightbulb_gear.jpg';
+import bg_landing from '../app/assets/img/light_bulb.png';
 
 const LandingPage = () => {
     return (
         <div>
-            <div 
+            {/* <div 
                 className="w-[100vw] h-[40vh] sm:h-[50vh] md:h-[70vh] lg:h-[80vh] xl:h-[90vh] 2xl:h-[100vh] bg-no-repeat bg-cover" 
-                style={{backgroundImage: ` url(${ bg_landing})`}}>
-                    <div className="container mx-auto text-gray-800 font-roboto_slab py-10 sm:py-20 md:py-30 lg:py-40">
-                        <h1 className="py-5 px-5 flex-initial w-1/2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">My name is Jeff Turner.</h1>
-                        <h1 className="py-5 px-5 flex-initial w-1/2 text-xl sm:text-2xl md:text-3xl lg:text-4xl">I am ready to take your ideas and put them into motion.</h1>
+                style={{backgroundImage: ` url(${ bg_landing})`}}></div> */}
+            <div 
+                className="bg-gradient-to-b from-stone-200 from-10% via-stone-500 via-75% to-stone-600 to-90%">
+                    <div className="container m-auto grid grid-cols-2">
+                        <div className="container mx-auto text-gray-800 font-roboto_slab py-10 sm:py-20 md:py-30 lg:py-40">
+                            <h1 className="py-5 px-5 flex-initial text-2xl sm:text-3xl md:text-4xl lg:text-5xl">My name is Jeff Turner.</h1>
+                            <h1 className="py-5 px-5 flex-initial text-xl sm:text-2xl md:text-3xl lg:text-4xl">I take your ideas and put them into motion.</h1>
+                        </div>
+                        <div className="container mx-auto max-w-sm overflow-hidden mt-5 sm:mt-10 mb-60">
+                            <img className="" src={bg_landing} />
+                        </div>
                     </div>
+
             </div>
         </div>
     );
