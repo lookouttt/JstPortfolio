a7ae6d56bffe2a51d0c3f2cb547e5b62a1828fa9
lookouttt <lookoutt@hotmail.com>
2023-04-22 01:17:12 -0700

    Made a few styling changes. Add hrefs for social icons.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,25 @@ import {FaGithubSquare, FaLinkedin, FaEnvelope} from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div className="flex items-center flex-wrap bg-amber-50 z-50">
-            <div className="flex items-center flex-shrink-0 mr-6">
+        <div className="flex items-center justify-between bg-amber-50 z-50">
+            <div className="flex items-center flex-shrink-0 ml-6">
                 <img className='p-1 w-30 h-16 md:h-20' src={ jst_logo }></img>
             </div>
-            <div className={"w-full block md:flex md:items-center md:w-auto"}>
-                <div className="inline-block px-2 py-2 leading-none text-md md:flex-grow mt-4">
-                    <FaGithubSquare className='h-6 w-6'/>
+            <div className={"block mr-6 w-auto"}>
+                <div className="inline-block px-5 py-2 leading-none text-md mt-4">
+                    <a href="https://github.com/lookouttt">
+                        <FaGithubSquare className='h-6 w-6'/>
+                    </a>
                 </div>
-                <div className="inline-block px-2 py-2 leading-none text-md md:flex-grow mt-4">
-                    <FaLinkedin className='h-6 w-6'/>
+                <div className="inline-block px-5 py-2 leading-none text-md mt-4">
+                    <a href="https://www.linkedin.com/in/jeff-turner-887815186/">
+                        <FaLinkedin className='h-6 w-6'/>
+                    </a>
                 </div>
-                <div className="inline-block px-2 py-2 leading-none text-md md:flex-grow mt-4">
-                    <FaEnvelope className='h-6 w-6'/>
+                <div className="inline-block px-5 py-2 leading-none text-md mt-4">
+                    <a href="mailto:jeff@jeffreysturner.com">
+                        <FaEnvelope className='h-6 w-6'/>
+                    </a>
                 </div>
             </div>
         </div>
