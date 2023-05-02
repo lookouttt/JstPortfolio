3b5984e040671f16a062ba08a24ebbabf6d28328
lookouttt <lookoutt@hotmail.com>
2023-05-02 02:16:47 -0700

    Capturing the changes to the About Me page. Still need to fine-tune the responsive styling.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,17 +8,46 @@ const AboutPage = () => {
                 <div className="container mx-auto text-center text-gray-400 pt-1">
                     <h1 className="py-5 px-2 flex-initial w-100">About Me</h1>
                 </div>
-                <div className="container m-auto">
-                    <div className="mx-5 overflow-hidden float-left mt-3 sm:mt-6">
-                        <img className="rounded-full h-48 sm:h-64 lg:h-80 xl:h-96" src={fej_pic} />
+                <div className="container m-auto grid grid-cols-1 md:grid-cols-3">
+                    <div className="mx-5 overflow-hidden mt-3 sm:mt-6 md:col-start-1 md:col-end-2">
+                        <img className="rounded-full h-32 sm:h-64 lg:h-80 xl:h-96" src={fej_pic} />
                     </div>
-                    <div className="container mx-auto text-gray-400 mt-3 sm:mt-6 pb-5 sm:pb-10">
-                        <p className="py-2 px-4 flex-initial w-100 text-sm sm:text-base md:text-lg">Let me tell you a little about myself.  I am a former software engineer who spent 20 years in the material handling industry. I spent the first decade of my time there traveling the country bringing multi-million-dollar conveyor systems to life. This helped me to develop the skills needed to meet customer needs within specified deadlines. During this time, I was involved in developing software for several new conveyor products. I also helped to mentor other engineers as they began their journey in the material handling industry.</p>
-                        <p className="py-2 px-4 flex-initial w-100 text-sm sm:text-base md:text-lg">During my second decade in that industry, I moved into R&D and support roles. I was a key contributor on two significant new conveyor products, one of which now has a US patent on it. I also expanded my knowledge of other software development areas including relational databases, version control systems, and agile software development.</p>
-                        <p className="py-2 px-4 flex-initial w-100 text-sm sm:text-base md:text-lg">Over my two decades in the material handling industry, I was constantly learning and honing my software development and troubleshooting skills. During that time, I have worked extensively with many software technologies including C, C++, C#, and SQL. I also had opportunities to work occasionally with Java, PHP, HTML, CSS and Javascript.</p>
-                        <p className="py-2 px-4 flex-initial w-100 text-sm sm:text-base md:text-lg">After 20 years in the industry, I chose to take a career break so that my family and I could relocate back to where my wife and I lived during our formative years. This break also allowed me to take care of some family matters that would have otherwise been difficult to attend to.</p>
-                        <p className="py-2 px-4 flex-initial w-100 text-sm sm:text-base md:text-lg">As the end of 2022 was approaching, I decided it was time to get back into the software industry. I decided, however, that this time I would like to pivot into the world of web and app development. To prepare for this, I enrolled in the Nucamp Full Stack Web and App Development bootcamp. I completed this bootcamp in March of 2023 with a Certificate of Honor within the program. This program allowed me to get more familiar with current development technologies including Javascript, Bootstrap, React, React Native, Node.js, Express, and MongoDB.</p>
-                        <p className="py-2 px-4 flex-initial w-100 text-sm sm:text-base md:text-lg">Over the years, I have developed a wealth of knowledge across many facets of the software development industry. Now I would love the opportunity to use that knowledge to help you. Drop me a line below and let me know how I can take your ideas and put them into motion. And click the below link to my resume if you're interested in finding out more about my experience. </p>
+                    <div className="container md:col-start-2 md:col-span-2 px-5 md:px-10 lg:px-16 xl:px-20 text-gray-400 mt-3 sm:mt-6 pb-5 sm:pb-10">
+                        <p className="py-2 px-4 flex-initial w-100 text-justify text-sm sm:text-base md:text-lg">Let me tell you a little bit about myself. I am a software engineer who spent 20 years in the material handling industry. I spent the first decade of my time there traveling the country bringing multi-million-dollar conveyor systems to life. During my second decade in that industry, I worked in various R&D and support roles. My varied roles over the last 25 years has helped me to develop many skills, including:</p>
+                        <p className="py-2 px-4 flex-initial w-100 text-justify text-sm sm:text-base md:text-lg">
+                            <ul className="list-disc ml-10">
+                                <li>Meeting customers’ needs on time and on budget</li>
+                                <li>Exceptional hardware and software troubleshooting skills</li>
+                                <li>Continuously learning new software languages, frameworks, and technologies, including:</li>
+                                    <ul className="columns-4 list-disc marker:text-slate-300 ml-10">
+                                        <li>C</li>
+                                        <li>C++</li>
+                                        <li>C#</li>
+                                        <li>JavaScript</li>
+                                        <li>Python</li>
+                                        <li>Java</li>
+                                        <li>PHP</li>
+                                        <li>Git</li>
+                                        <li>SVN</li>
+                                        <li>MS SQL</li>
+                                        <li>PostgreSQL</li>
+                                        <li>MongoDB</li>
+                                        <li>HTML</li>
+                                        <li>CSS</li>
+                                        <li>Tailwind CSS</li>
+                                        <li>Bootstrap</li>
+                                        <li>React</li>
+                                        <li>React Native</li>
+                                        <li>Node.js</li>
+                                        <li>Express</li>
+                                    </ul>
+                                <li>Working within numerous agile and non-agile development teams to produce new software products, including one where I am a joint U.S. patent holder</li>
+                                <li>Working on software across a variety of specialties including conveyor control systems, user interfaces, host communication systems, database systems, and web development</li>
+                            </ul>
+                        </p>
+                        <p className="py-2 px-4 flex-initial w-100 text-justify text-sm sm:text-base md:text-lg">After 20 years in the material handling industry, I chose to take a career break so that I could relocate closer to family. I remodeled two houses during this time. I was able to assist with several important family matters. I became our family’s primary caregiver during the COVID pandemic. I also took time to enhance my knowledge during this time by learning new skills involving currency trading, data analysis, and web and app development.</p>
+                        <p className="py-2 px-4 flex-initial w-100 text-justify text-sm sm:text-base md:text-lg">As the end of 2022 approached, I decided it was time for me to get back into the software industry. In March of 2023, I completed the Nucamp Full Stack Web and App Development bootcamp with a Certificate of Honor. I greatly enjoyed learning these new technologies, and I look forward to using them as I pivot into web and app development.</p>
+                        <p className="py-2 px-4 flex-initial w-100 text-justify text-sm sm:text-base md:text-lg">I have developed a wealth of knowledge across many facets of the software development industry over the last 25 years. I would love the opportunity to use that knowledge to help you. Drop me a line below and let me know how I can take your ideas and put them into motion.</p>
                     </div>
                 </div>
                 <div className="container mx-auto text-center pb-10 sm:pb-20">
